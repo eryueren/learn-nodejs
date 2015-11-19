31d038c6407065bb65a317a204e6568f10b06cfc
John <eryueren@163.com>
2015-11-19 16:47:43 +0800

    add example02/foo/foo.js

--- example01/server/server.js
+++ example01/server/server.js
@@ -3,4 +3,3 @@ h.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
     res.end("开始我的第一Node程序\n");
 }).listen(3000, '127.0.0.1');
-console.log("开始我的第一Node程序！");
---
+++ example02/foo/foo.js
@@ -0,0 +1,2 @@
+var f=require("underscore");
+f.ea
